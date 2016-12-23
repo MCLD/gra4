5550369fa41bc7a4b663f95936c259901d35ff2f
Dan <DanielWilcox@mcldaz.org>
2016-12-23 13:49:25 -0700

    Add resetting activeuser to MC filter

--- src/GRA.Controllers/Filter/MissionControlFilter.cs
+++ src/GRA.Controllers/Filter/MissionControlFilter.cs
@@ -1,4 +1,5 @@
 ﻿using GRA.Domain.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Threading.Tasks;
@@ -14,17 +15,24 @@ namespace GRA.Controllers.Filter
             _mailService = Require.IsNotNull(mailService, nameof(mailService));
         }
 
-        public async Task OnResourceExecutionAsync(ResourceExecutingContext context, 
+        public async Task OnResourceExecutionAsync(ResourceExecutingContext context,
             ResourceExecutionDelegate next)
         {
             var httpContext = context.HttpContext;
+            var userId = new UserClaimLookup(httpContext.User).GetId(ClaimType.UserId);
+            var activeId = httpContext.Session.GetInt32(SessionKey.ActiveUserId);
+            if (userId != activeId)
+            {
+                httpContext.Session.SetInt32(SessionKey.ActiveUserId, userId);
+            }
+            
             if (httpContext.User.HasClaim(GRA.ClaimType.Permission,
                 GRA.Domain.Model.Permission.ReadAllMail.ToString()))
             {
                 httpContext.Items[ItemKey.UnreadCount] = await _mailService.GetAdminUnreadCountAsync();
             }
 
-                await next();
+            await next();
         }
     }
 }
