32dbd3ef1a52a234cc60271f292cf3d81add472b
Dan <DanielWilcox@mcldaz.org>
2017-01-31 11:16:30 -0700

    Add adding existing users to household

---
+++ src/GRA.Controllers/ViewModel/Profile/HouseholdExistingViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace GRA.Controllers.ViewModel.Profile
+{
+    public class HouseholdExistingViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
