6be5e115e2b800cc369bbd7318eb1c29c329f698
Harald Nagel <haraldnagel@users.noreply.github.com>
2017-12-13 15:48:28 -0700

    Add back-end for site-level settings
    - Add site-level settings back-end functionality
    - Remove application configuration for hiding secret codes

---
+++ src/GRA/SiteSettingKey.cs
@@ -0,0 +1,13 @@
+﻿
+namespace GRA
+{
+    public struct SiteSettingKey
+    {
+        public struct SecretCode
+        {
+            // Currently: if this is set (i.e. not null), hide the secret code entry
+            // TODO make this truly disable secret codes for the site
+            public const string Disable = "SecretCode.Disable";
+        }
+    }
+}
